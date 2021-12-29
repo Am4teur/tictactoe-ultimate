c786b91d051ab3ed9521600e61e3452df5ac9449
Daniel Castro <daniel.castro@digitalhub.man>
2021-12-29 22:05:29 +0000

    Added dark/light mode toggler to settings

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, HStack, Switch, useColorMode } from "native-base";
+
+export default function ThemeToggle() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <HStack space={2} alignItems="center">
+      <Text>Dark</Text>
+      <Switch
+        isChecked={colorMode === "light"}
+        onToggle={toggleColorMode}
+      ></Switch>
+      <Text>Light</Text>
+    </HStack>
+  );
+}
