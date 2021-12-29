3b7081c20a9c861f87fc07f03fa2ce18db73205a
Daniel Castro <daniel.castro@digitalhub.man>
2021-12-29 11:05:56 +0000

    Changed App to root folder

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppContainer from "./src/components/AppContainer";
+import Router from "./src/index";
+
+export default function App() {
+  return (
+    <AppContainer>
+      <Router />
+    </AppContainer>
+  );
+}
