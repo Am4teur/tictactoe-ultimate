f61979472a919595689c66b9b0a66a2ba18d3664
Daniel Castro <daniel.castro@digitalhub.man>
2021-12-30 20:27:14 +0000

    Improve the square style

--- src/components/TTTGame/Square.tsx
+++ src/components/TTTGame/Square.tsx
@@ -10,7 +10,13 @@ type Props = {
 const Square = ({ value, showValue, i, j }: Props) => {
   return (
     <Box borderWidth={2} borderColor="gray.800" borderStyle="solid" w="8" h="8">
-      <Button onPress={() => showValue(i, j)} variant="unstyled">
+      <Button
+        onPress={() => showValue(i, j)}
+        variant="unstyled"
+        p="0"
+        w="full"
+        h="full"
+      >
         {value !== "N" ? value : null}
       </Button>
     </Box>
