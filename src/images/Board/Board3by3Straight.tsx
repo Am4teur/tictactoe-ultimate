fd1934a236733330bbba3a6b07e01536c888ca4e
Daniel Castro <daniel.castro@digitalhub.man>
2022-02-01 20:54:49 +0000

    Make stroke width adjustable for board svg's

--- src/images/Board/Board3by3Straight.tsx
+++ src/images/Board/Board3by3Straight.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Svg, { SvgProps, G, Path } from "react-native-svg";
 
-const Board3by3Straight = ({ stroke }: SvgProps) => (
+const Board3by3Straight = ({ stroke, strokeWidth = 10 }: SvgProps) => (
   <Svg viewBox="0 0 300 300" width="100%" height="100%">
-    <G stroke={stroke} strokeWidth={10} strokeLinecap="round">
+    <G stroke={stroke} strokeWidth={strokeWidth} strokeLinecap="round">
       <Path d="M100 10v280" />
       <Path d="M10 100h280" />
       <Path d="M200 10v280" />
